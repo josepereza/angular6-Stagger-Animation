3cda12fa08945ad4859a1d04ad5d35bf1a910731
Avinash <avinashg12291992@gmail.com>
2018-07-18 22:24:54 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', style({
+          opacity: 0
+        }),
+        {optional: true}
+      ),
+      query(':enter', stagger('300ms', [
+        animate('1s ease-in', keyframes([
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+          style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+        ]))]), {optional: true}),
+        query(':leave', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+        ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  listItems = [];
+  @ViewChild('pokemonVal') pokemonVal: ElementRef;
+  constructor() {
+    this.listItems = ['Pikachu', 'Charmander', 'Bulbasaur', 'Squirtle'];
+  }
+  addItem(pokemon) {
+    console.log(this.pokemonVal);
+    this.listItems.push(pokemon.value);
+  }
+  removeItem() {
+    this.listItems.pop();
+  }
 }
